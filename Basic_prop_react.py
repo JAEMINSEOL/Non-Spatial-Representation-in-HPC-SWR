20b586f58cafbd3916abae5820a348514c6c5c59
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2021-12-21 18:02:55 +0900

    0. Reactivated cell들이 갖는 특성 확인

---
+++ Basic_prop_react.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 21 17:28:19 2021
+
+0. Reactivated cell들이 갖는 특성 확인
+
+@author: user
+"""
+
+#%% import libraries
+import numpy as np 
+import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools as ite
+
+#%%
