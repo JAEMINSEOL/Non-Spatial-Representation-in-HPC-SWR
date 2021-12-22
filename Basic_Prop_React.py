73558552143d99b9fb73adc339b22356b2966f8f
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2021-12-22 17:56:52 +0900

    0. Reactivated cell들이 갖는 특성 확인

--- Basic_Prop_React.py
+++ Basic_Prop_React.py
@@ -16,3 +16,28 @@ import seaborn as sns
 import itertools as ite
 
 #%%
+
+temp = df_unit_valid.loc[:,'NumRipples_1':'NumRipples_4'].sum(axis=1)
+temp = pd.concat([temp,df_unit_valid.Session],axis=1)
+temp = temp.rename(columns={0:'ReactRips'})
+for thisSID in temp.Session.unique():
+    temp.loc[temp['Session']==thisSID,'TotRips']=sum(df_rip_valid.Session==thisSID)
+#%%
+plt.figure(figsize=(10,8))
+ax = sns.histplot(temp.ReactRips / temp.TotRips,stat='probability')
+ax.set_xlabel('Participation in Ripple')
+
+#%%
+clist = ['r','b','y','g']
+f,axes=plt.subplots(2,2,figsize=(10,8),sharex=True,sharey=True)
+for Cxt in range(1,5):
+    temp = df_unit_valid.loc[:,f'NumRipples_{Cxt}']
+    temp = pd.concat([temp,df_unit_valid.Session],axis=1)
+    
+    for thisSID in temp.Session.unique():
+        temp.loc[temp['Session']==thisSID,'TotRips']=sum((df_rip_valid.Session==thisSID) & (df_rip_valid.Context==Cxt))
+
+
+    sns.histplot(temp[f'NumRipples_{Cxt}'] / temp.TotRips,stat='probability', color=clist[Cxt-1],ax=axes[divmod(Cxt-1,2)])
+    
+plt.xlabel('Participation in Ripple')
