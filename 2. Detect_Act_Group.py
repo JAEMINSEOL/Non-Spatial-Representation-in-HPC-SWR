4205cea413f18f12779c881801ae2b77aa245671
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2021-12-23 15:37:33 +0900

    Update and rename Parsing_React_Units.py to 2. Detect_Act_Group.py

--- 2. Detect_Act_Group.py
+++ 2. Detect_Act_Group.py
@@ -2,8 +2,10 @@
 """
 Created on Thu Dec  9 10:43:10 2021
 
-1. Reactivated group의 RDI 분포 확인
-@author: user
+2. Activated group의 RDI 분포 확인
+input: ReactTable_valid, RipplesTable_valid, UnitsTable_valid
+output: Active group detecting plots
+@author: JM_Seol
 """
 
 #%% import libraries
@@ -15,76 +17,34 @@ import seaborn as sns
 import itertools as ite
 #%% import data
 ROOT_data = 'D:/HPC-SWR project/Processed Data'
-df_rip=pd.DataFrame()
-df_unit=pd.DataFrame()
-df_react=pd.DataFrame()
+thisRID=561
+thisSID='all'
+thisRegion='CA1'
+   
+df_rip_valid = pd.read_excel(f'{ROOT_data}/RipplesTable_r{thisRID}_{thisSID}_{thisRegion}_v.xlsx')
+df_unit_valid = pd.read_excel(f'{ROOT_data}/UnitsTable_r{thisRID}_{thisSID}_{thisRegion}_v.xlsx')
+df_act_valid = pd.read_excel(f'{ROOT_data}/ReactTable_r{thisRID}_{thisSID}_{thisRegion}_v.xlsx')
 
-for thisSID in range(5):
-    thisRID = 561
-    thisSID = thisSID+1
-    thisRegion = 'CA1'
-    
-    df_rip_temp = pd.read_excel(f'{ROOT_data}/RipplesTable_r{thisRID}_s{str(thisSID).zfill(2)}_{thisRegion}.xlsx')
-    df_unit_temp = pd.read_excel(f'{ROOT_data}/UnitsTable_r{thisRID}_s{str(thisSID).zfill(2)}_{thisRegion}.xlsx')
-    df_react_temp = pd.read_excel(f'{ROOT_data}/ReactTable_r{thisRID}_s{str(thisSID).zfill(2)}_{thisRegion}.xlsx')
-    
-    df_rip=df_rip.append(df_rip_temp, ignore_index=True)
-    df_unit=df_unit.append(df_unit_temp, ignore_index=True)
-    df_react=df_react.append(df_react_temp, ignore_index=True)
 #%% data processing
-df_rip['RipID'] = df_rip['RipID'].astype(str).str.zfill(4)
-df_rip['Session'] = df_rip['Session'].astype(str).str.zfill(1)
-df_rip['RipID'] = df_rip['Session'] + '-' + df_rip['RipID']
-
-df_unit['Session'] = df_unit['Session'].astype(str).str.zfill(1)
-df_unit['TT'] = df_unit['TT'].astype(str).str.zfill(2)
-df_unit['Unit'] = df_unit.Unit.astype(str).str.zfill(2)
-df_unit['TT-Unit'] = df_unit['Session'] + '-' + df_unit['TT'] +  '-' + df_unit['Unit']
-
-df_react['RipID'] = df_react['RipID'].astype(str).str.zfill(4)
-df_react['SessionID'] = df_react['SessionID'].astype(str).str.zfill(1)
-df_react['TT'] = df_react['TT'].astype(str).str.zfill(2)
-df_react['UnitID'] = df_react.UnitID.astype(str).str.zfill(2)
-df_react['TT-Unit'] = df_react['SessionID'] + '-' + df_react['TT'] +  '-' + df_react['UnitID']
-df_react['RipID'] = df_react['SessionID'] + '-' + df_react['RipID']
-
-valid_rips = df_rip.RipID[df_rip.Filter>0]
-df_react_valid = df_react[df_react['RipID'].isin(valid_rips) & df_react['Type']>0]
-df_unit_valid = df_unit[df_unit['TT-Unit'].isin(df_react_valid['TT-Unit'])]
-
-
 df_unit_valid_dv = df_unit_valid[(df_unit_valid.PeakArea==3)]
 
+df_act_valid_dv = df_act_valid[df_act_valid['TT-Unit'].isin(df_unit_valid_dv['TT-Unit'])]
 
-df_react_valid_dv = df_react_valid[df_react_valid['TT-Unit'].isin(df_unit_valid_dv['TT-Unit'])]
+df_act_pivot_dv = pd.pivot_table(df_act_valid_dv, index='TT-Unit', columns = 'RipID',values='Region',aggfunc='count')
+df_act_pivot_dv = ~df_act_pivot_dv.isna()*1
+df_act_pivot_dv[df_act_pivot_dv==0]=np.nan
 
-df_react_pivot_dv = pd.pivot_table(df_react_valid_dv, index='TT-Unit', columns = 'RipID',values='Region',aggfunc='count')
-df_react_pivot_dv = ~df_react_pivot_dv.isna()*1
-df_react_pivot_dv[df_react_pivot_dv==0]=np.nan
+df_act_pivot_dv_rdi =df_act_pivot_dv.mul(df_unit_valid_dv.RDI_ZB.values,axis=0)
 
-df_react_pivot_dv_rdi =df_react_pivot_dv.mul(df_unit_valid_dv.RDI_ZB.values,axis=0)
-
-df_rip['meanRDI_ZB'] = 0
-df_rip['meanRDI_PM'] = 0
-df_rip['meanRDI_LR'] = 0
-for i in range(df_rip.shape[0]):
-    thisRipID = df_rip.RipID[i]
-    thisUnitID = df_react_valid['TT-Unit'][df_react_valid['RipID']==thisRipID]
-    thisUnits = df_unit_valid[df_unit_valid['TT-Unit'].isin(thisUnitID)]
-    df_rip['meanRDI_ZB'].iloc[i] = np.mean(thisUnits.RDI_ZB)
-    df_rip['meanRDI_PM'].iloc[i] = np.mean(thisUnits.RDI_PM)
-    df_rip['meanRDI_LR'].iloc[i] = np.mean(thisUnits.RDI_LR)
-    
-df_rip_valid = df_rip[df_rip['Filter']>0]
 
 #
 unit=list()
-for i in range(df_react_pivot_dv.shape[0]):
-    temp = df_react_pivot_dv.iloc[i,:]
+for i in range(df_act_pivot_dv.shape[0]):
+    temp = df_act_pivot_dv.iloc[i,:]
     u = np.where(~np.isnan(temp))
     unit.append(u)
 #
-pivot_overlap=pd.DataFrame(index = df_react_pivot_dv.index, columns=df_react_pivot_dv.index)
+pivot_overlap=pd.DataFrame(index = df_act_pivot_dv.index, columns=df_act_pivot_dv.index)
 for i in range(len(unit)):
     for j in range(len(unit)):
         pivot_overlap.iat[i,j] = len(np.intersect1d(unit[i],unit[j]))
@@ -93,19 +53,13 @@ for i in range(len(unit)):
 
 
 #%% make react sheet & save
-df_unit_valid_dv=df_unit_valid_dv.set_axis(df_unit_valid_dv['TT-Unit'],axis=0)
-df_unit_valid=df_unit_valid.set_axis(df_unit_valid['TT-Unit'],axis=0)
-df_rip_valid=df_rip_valid.set_axis(df_rip_valid['RipID'],axis=0)
-df_react_valid=df_react_valid[df_react_valid['RipID'].isin(df_rip_valid['RipID'])]
+
 # RDI distribution plot
-
-
-
-x = df_unit_valid.loc[df_react_valid['TT-Unit'],['TT-Unit','PeakArea','RDI_ZB','RDI_PM','RDI_LR']]
+x = df_unit_valid.loc[df_act_valid['TT-Unit'],['TT-Unit','PeakArea','RDI_ZB','RDI_PM','RDI_LR']]
 x=x.reset_index(drop=True)
-y = df_react_valid.loc[:,['RipID','SpkTime']]
+y = df_act_valid.loc[:,['RipID','SpkTime']]
 y=y.reset_index(drop=True)
-h = df_rip_valid.loc[df_react_valid['RipID'],['Context','meanRDI_ZB','meanRDI_PM','meanRDI_LR']]
+h = df_rip_valid.loc[df_act_valid['RipID'],['Context','meanRDI_ZB','meanRDI_PM','meanRDI_LR']]
 h=h.reset_index(drop=True)
 dat = pd.concat([x,y,h],axis=1)
 dat['RDI_LR']=dat['RDI_LR'].fillna(0)
@@ -117,9 +71,7 @@ dat['rank']=dat[['RipID']].apply(tuple,axis=1).rank(method='dense')
 dat=dat.sort_values('rank')
 
 dat.to_excel(f'{ROOT_data}/ReactTable_r{thisRID}_all_{thisRegion}_2.xlsx')
-df_unit_valid.to_excel(f'{ROOT_data}/UnitTable_r{thisRID}_all_{thisRegion}.xlsx')
-df_rip_valid.to_excel(f'{ROOT_data}/RipplesTable_r{thisRID}_all_{thisRegion}.xlsx')
-df_react_valid.to_excel(f'{ROOT_data}/ReactTable_r{thisRID}_all_{thisRegion}.xlsx')
+
 
 #%% Wilcoxon signed rank sum test & plotting
 Targ='PM'
@@ -185,16 +137,16 @@ for i in cxtshade:
 #%% 
 
 
-df_react_comb = pd.DataFrame(columns=[0,1])
-for thisRipID in df_react_valid_dv['RipID'].unique():
-    temp = df_react_valid_dv[df_react_valid_dv['RipID']==thisRipID] 
+df_act_comb = pd.DataFrame(columns=[0,1])
+for thisRipID in df_act_valid_dv['RipID'].unique():
+    temp = df_act_valid_dv[df_act_valid_dv['RipID']==thisRipID] 
     for subset in ite.combinations(temp['TT-Unit'],2):       
-        df_react_comb=df_react_comb.append(pd.DataFrame(list(subset)).T)
+        df_act_comb=df_act_comb.append(pd.DataFrame(list(subset)).T)
 
 typ = ['RDI_ZB','RDI_PM']
-for i in range(len(df_react_comb)):
-    u1 = df_react_comb.iloc[i,0]
-    u2 = df_react_comb.iloc[i,1]
+for i in range(len(df_act_comb)):
+    u1 = df_act_comb.iloc[i,0]
+    u2 = df_act_comb.iloc[i,1]
     x = [df_unit_valid.loc[df_unit_valid['TT-Unit']==u1,typ[0]].iloc[0], 
             df_unit_valid.loc[df_unit_valid['TT-Unit']==u2,typ[0]].iloc[0]]
     y= [df_unit_valid.loc[df_unit_valid['TT-Unit']==u1,typ[1]].iloc[0],
