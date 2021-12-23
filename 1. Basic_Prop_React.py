7c1c853941179e6c1fa5f26552a83821b0adec39
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2021-12-23 17:59:11 +0900

    Update 1. Basic_Prop_React.py

--- 1. Basic_Prop_React.py
+++ 1. Basic_Prop_React.py
@@ -24,30 +24,45 @@ thisRegion='CA1'
 
 df_rip = pd.read_excel(f'{ROOT_data}/RipplesTable_r{thisRID}_{thisSID}_{thisRegion}.xlsx')
 df_unit = pd.read_excel(f'{ROOT_data}/UnitsTable_r{thisRID}_{thisSID}_{thisRegion}.xlsx')
-df_react = pd.read_excel(f'{ROOT_data}/ReactTable_r{thisRID}_{thisSID}_{thisRegion}.xlsx')
+df_act = pd.read_excel(f'{ROOT_data}/ActTable_r{thisRID}_{thisSID}_{thisRegion}.xlsx')
 
 df_rip_valid = pd.read_excel(f'{ROOT_data}/RipplesTable_r{thisRID}_{thisSID}_{thisRegion}_v.xlsx')
 df_unit_valid = pd.read_excel(f'{ROOT_data}/UnitsTable_r{thisRID}_{thisSID}_{thisRegion}_v.xlsx')
-df_react_valid = pd.read_excel(f'{ROOT_data}/ReactTable_r{thisRID}_{thisSID}_{thisRegion}_v.xlsx')
+df_act_valid = pd.read_excel(f'{ROOT_data}/ActTable_r{thisRID}_{thisSID}_{thisRegion}_v.xlsx')
 
-
+df_unit = df_unit[df_unit.Type!=0]
+#%% RDI, field peak position distribution
+f,axes=plt.subplots(2,2,figsize=(10,8),sharey=True)
+for i in range(1,5):
+   sns.histplot(df_unit.iloc[:,i+8], stat='probability',color='k',ax=axes[divmod(i-1,2)],binwidth=3 if i==1 else 0.1)
+   sns.histplot(df_unit_valid.iloc[:,i+8],stat='probability', color='r',ax=axes[divmod(i-1,2)],binwidth=3 if i==1 else 0.1)
+   if i>1:
+       plt.xlim(-1,1)
 #%%
-
+temp=list(np.zeros(5))
+for i in range(1,6):
+    temp[i-1] = sum(df_rip_valid.Session==i)
+temp=pd.DataFrame(data=temp)
+plt.figure(figsize=(10,8))
+ax = sns.barplot(data=temp.T)
+ax.set_xticklabels(['Session1','Session2','Session3','Session4','Session5'])
+plt.ylabel('Ripples')
+#%% Ripple participation rate (all scenes)
 temp = df_unit_valid.loc[:,'NumRipples_1':'NumRipples_4'].sum(axis=1)
 temp = pd.concat([temp,df_unit_valid.Session],axis=1)
 temp = temp.rename(columns={0:'ReactRips'})
 for thisSID in temp.Session.unique():
     temp.loc[temp['Session']==thisSID,'TotRips']=sum(df_rip_valid.Session==thisSID)
-#%%
+
 plt.figure(figsize=(10,8))
 ax = sns.histplot(temp.ReactRips / temp.TotRips,stat='probability')
 ax.set_xlabel('Participation in Ripple')
 
-#%%
+#%% Ripple participation rate (each scene)
 clist = ['#5AB7D4','#F79334','#00506A','#9A4700']
 f,axes=plt.subplots(2,2,figsize=(10,8),sharex=True,sharey=True)
 for Cxt in range(1,5):
-    temp = df_unit_valid.loc[:,f'NumRipples_{Cxt}']
+    temp = df_unit_valid.loc[:,['TT-Unit', f'NumRipples_{Cxt}']]
     temp = pd.concat([temp,df_unit_valid.Session],axis=1)
     
     for thisSID in temp.Session.unique():
@@ -58,13 +73,5 @@ for Cxt in range(1,5):
     
 plt.xlabel('Participation in Ripple')
 
-#%%
 
-    
-f,axes=plt.subplots(2,2,figsize=(10,8))
-for i in range(1,5):
-   sns.histplot(df_unit.iloc[:,i+8], color='k',ax=axes[divmod(i-1,2)],stat='probability',binwidth=3 if i==1 else 0.1)
-   sns.histplot(df_unit_valid.iloc[:,i+8], color='r',ax=axes[divmod(i-1,2)],stat='probability',binwidth=3 if i==1 else 0.1)
-   if i>1:
-       plt.xlim(-1,1)
     
