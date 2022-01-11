88f7f5cc685b12a327d3480cafca5e6f1ecd7697
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-01-11 17:21:09 +0900

    Update 2. Detect_Act_Group.py

--- 2. Detect_Act_Group.py
+++ 2. Detect_Act_Group.py
@@ -26,32 +26,6 @@ df_rip_valid = pd.read_excel(f'{ROOT_data}/RipplesTable_r{thisRID}_{thisSID}_{th
 df_unit_valid = pd.read_excel(f'{ROOT_data}/UnitsTable_r{thisRID}_{thisSID}_{thisRegion}_v.xlsx')
 df_act_valid = pd.read_excel(f'{ROOT_data}/ActTable_r{thisRID}_{thisSID}_{thisRegion}_v.xlsx')
 
-#%% data processing
-df_unit_valid_dv = df_unit_valid[(df_unit_valid.PeakArea==3)]
-
-df_act_valid_dv = df_act_valid[df_act_valid['TT-Unit'].isin(df_unit_valid_dv['TT-Unit'])]
-
-df_act_pivot_dv = pd.pivot_table(df_act_valid_dv, index='TT-Unit', columns = 'RipID',values='Region',aggfunc='count')
-df_act_pivot_dv = ~df_act_pivot_dv.isna()*1
-df_act_pivot_dv[df_act_pivot_dv==0]=np.nan
-
-df_act_pivot_dv_rdi =df_act_pivot_dv.mul(df_unit_valid_dv.RDI_ZB.values,axis=0)
-
-
-#
-unit=list()
-for i in range(df_act_pivot_dv.shape[0]):
-    temp = df_act_pivot_dv.iloc[i,:]
-    u = np.where(~np.isnan(temp))
-    unit.append(u)
-#
-pivot_overlap=pd.DataFrame(index = df_act_pivot_dv.index, columns=df_act_pivot_dv.index)
-for i in range(len(unit)):
-    for j in range(len(unit)):
-        pivot_overlap.iat[i,j] = len(np.intersect1d(unit[i],unit[j]))
-        pivot_overlap.iat[j,i] = len(np.intersect1d(unit[i],unit[j]))
-    # pivot_overlap.iat[i,i] = 0
-
 
 #%% make react sheet & save
 
@@ -171,46 +145,4 @@ for i in cxtshade:
 cxtshade = dat_part[dat_part['Context']==4]['rank'].unique().astype(int)
 for i in cxtshade:
     ax.axvline(i, color='g', linewidth=5,alpha=0.3)
-    
-#%% 
-
-
-df_act_comb = pd.DataFrame(columns=[0,1])
-for thisRipID in df_act_valid_dv['RipID'].unique():
-    temp = df_act_valid_dv[df_act_valid_dv['RipID']==thisRipID] 
-    for subset in ite.combinations(temp['TT-Unit'],2):       
-        df_act_comb=df_act_comb.append(pd.DataFrame(list(subset)).T)
-
-typ = ['RDI_ZB','RDI_PM']
-for i in range(len(df_act_comb)):
-    u1 = df_act_comb.iloc[i,0]
-    u2 = df_act_comb.iloc[i,1]
-    x = [df_unit_valid.loc[df_unit_valid['TT-Unit']==u1,typ[0]].iloc[0], 
-            df_unit_valid.loc[df_unit_valid['TT-Unit']==u2,typ[0]].iloc[0]]
-    y= [df_unit_valid.loc[df_unit_valid['TT-Unit']==u1,typ[1]].iloc[0],
-        df_unit_valid.loc[df_unit_valid['TT-Unit']==u2,typ[1]].iloc[0]]
-    plt.plot(x, y, marker = 'o',color='k',alpha=0.03)
-plt.xlabel("RDI_ZB")
-plt.ylabel("RDI_PM")
-plt.show()        
-# ax=sns.scatterplot(y='RDI_LR',x='rank')
-#%% data plotting
-# sns.scatterplot(df_unit_valid_dv.RDI_ZB,df_unit_valid_dv.RDI_PM, hue = df_unit_valid.NumRipples_1,legend=0)
-# sns.color_palette("tab10")
-# plt.show()
-
-# unit co-reactivate heatmap
-pivot_overlap = pivot_overlap.astype('int64')
-mask = np.triu(np.ones_like(pivot_overlap, dtype=np.bool))
-sns.heatmap(pivot_overlap, cmap ='Blues', linewidths = 0.30, annot = True, mask=mask)
-
-
-#%%
-
-
-
-
-
-
-#%%
 
