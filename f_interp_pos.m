050016debf3dc4e0ecf60eedee1c775524c72d4b
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-02-24 12:47:48 +0900

    Create f_interp_pos.m

---
+++ f_interp_pos.m
@@ -0,0 +1,13 @@
+function pos = f_interp_pos(pos,range,c)
+
+temp = pos(range);
+x = find(~isnan(temp));
+y = temp(x);
+xq = [1:length(temp)]';
+
+yq = round(interp1(x,y,xq,'linear','extrap'));
+
+pos(range) = yq;
+pos(~range) = c;
+
+end
