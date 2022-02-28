35189f734af4252d90931232eb1e8cb3c692ea55
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-02-28 15:58:37 +0900

    Create Filt5_AddEnsembleInfo.m

---
+++ Filt5_AddEnsembleInfo.m
@@ -0,0 +1,40 @@
+Initial_SWRFilter_common;
+warning off
+ROOT.Rip = [ROOT.Mother '\Processed Data\ripples_mat'];
+ROOT.Behav = [ROOT.Mother '\Processed Data\behavior_mat'];
+ROOT.React = [ROOT.Mother '\Processed Data\react_mat'];
+ROOT.Save = [ROOT.Mother '\Processed Data'];
+
+Recording_region = readtable([ROOT.Info '\Recording_region_SWR.csv'],'ReadRowNames',true);
+
+thisRegion = 'CA1';
+
+RipplesTable = readtable([ROOT.Save '\RipplesTable_Behav.xlsx']);
+UnitsTable = readtable([ROOT.Save '\UnitsTable_filtered.xlsx']);
+ReactTable = readtable([ROOT.Save '\ReactTable.xlsx']);
+
+
+for clRip = 1:size(RipplesTable,1)
+    RipID= cell2mat(RipplesTable.RippleID(clRip));
+    id = find(cellfun(cellfind(RipID),(ReactTable.RippleID)));
+    thisReactTable = ReactTable(id,:);
+    
+    unit=[];
+    thisUnitTable =[];
+    for clUnit=1:size(thisReactTable,1)
+        uid = cell2mat(thisReactTable.UnitID(clUnit));
+        unit(clUnit) = str2double(uid(8:9))*10^2+str2double(uid(11:12));
+        id = find(cellfun(cellfind(uid),(UnitsTable.UnitID)));
+        thisUnitTable = [thisUnitTable; UnitsTable(id(1),:)];
+        thisReactTable.SI(clUnit) = UnitsTable.SpaInfoScore1D(id(1));
+        thisReactTable.PeakBin(clUnit) = UnitsTable.PeakBin(id(1));
+    end
+    
+    if ~isempty(unit)
+        [unq_unit,ia,ic] = unique(unit','rows');
+        RipplesTable.Spikes(clRip) = length(unit);
+        RipplesTable.Ensemble_all(clRip) = length(unq_unit);
+        RipplesTable.Ensemble_PC(clRip) = sum(thisUnitTable.SpaInfoScore1D(ia)>=0.5);
+        RipplesTable.Ensemble_StemPC(clRip) = sum((thisUnitTable.SpaInfoScore1D(ia)>=0.5) & (thisUnitTable.PeakBin(ia)>1));
+    end
+end
