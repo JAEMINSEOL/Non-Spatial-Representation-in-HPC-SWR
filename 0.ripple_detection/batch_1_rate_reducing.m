62400afa9823ba885f8c168cdad30aae4f003363
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-03-21 21:30:46 +0900

    Delete 0.ripple_detection directory

--- 0.ripple_detection/batch_1_rate_reducing.m
+++
@@ -1,8 +0,0 @@
-Initial_SWRFilter_common;
-
-thisRID = 551;
-raw_dir = ['Y:\EPhysRawData\CA1,CA3,V2L Recording_VR_JS\Rat' jmnum2str(thisRID,3) ];
-save_dir = 'F:\EPhysRawData\RawData';
-for thisSID = 1:4
-RateReducer(thisRID, thisSID, raw_dir, save_dir)
-end
