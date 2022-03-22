7424246fcd72bf3ef030ff31cd59600b70365c59
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-03-22 10:08:53 +0900

    Update batch_1_rate_reducing.m

--- Analysis Set/0.ripple_detection/batch_1_rate_reducing.m
+++ Analysis Set/0.ripple_detection/batch_1_rate_reducing.m
@@ -4,5 +4,5 @@ thisRID = 551;
 raw_dir = ['Y:\EPhysRawData\CA1,CA3,V2L Recording_VR_JS\Rat' jmnum2str(thisRID,3) ];
 save_dir = 'F:\EPhysRawData\RawData';
 for thisSID = 1:4
-RateReducer(thisRID, thisSID, raw_dir, save_dir)
+RateReducer(thisRID, thisSID, raw_dir, save_dir,'AG')
 end
