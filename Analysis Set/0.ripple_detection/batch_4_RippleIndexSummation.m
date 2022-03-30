919fcc451bb9eceb27a8f4176a90584941efc3b1
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-03-30 17:02:10 +0900

    Update batch_4_RippleIndexSummation.m

--- Analysis Set/0.ripple_detection/batch_4_RippleIndexSummation.m
+++ Analysis Set/0.ripple_detection/batch_4_RippleIndexSummation.m
@@ -5,7 +5,7 @@ ROOT.Save = [ROOT.Mother '\Processed Data\ripples_mat\R0'];
 Recording_region = readtable([ROOT.Info '\Recording_region_SWR.csv'],'ReadRowNames',true);
 SessionList = readtable([ROOT.Info '\SessionList_SWR.xlsx'],'ReadRowNames',false);
 thisRegion = 'CA1';
-Experimenter = ['LSM'];
+Experimenter = {'LSM','JS','SEB'};
 
 
 
@@ -13,19 +13,21 @@ fd = dir(ROOT.Save);
 
 RipplesTable_all = table;
 
-for i=1:size(SessionList,1)
-    if SessionList.include(i) && ismember(SessionList.experimenter(i),Experimenter)
-        thisSID = [jmnum2str(SessionList.rat(i),3) '-' jmnum2str(SessionList.session(i),2)];
+for sid=1:size(SessionList,1)
+    if SessionList.include(sid) & ismember(SessionList.experimenter{sid},Experimenter) 
+        thisSID = [jmnum2str(SessionList.rat(sid),3) '-' jmnum2str(SessionList.session(sid),2)];
     Recording_region_TT = Recording_region({thisSID},:);
     
     TargetTT = find(cellfun(cellfind(thisRegion),table2array(Recording_region_TT)'));
-    
-    Ripples = load([ROOT.Save '\' thisSID '.mat']);
+    if size(TargetTT,1)<2, continue; end
+    load([ROOT.Save '\' thisSID '.mat']);
     %%
     for t1=1:length(TargetTT)
+        try
         t=TargetTT(t1);
-        MaxT(t,1) = max(Ripples.Ripples.(['TT' num2str(t)]).index(:,2));
-        MaxT(t,2) = max(Ripples.Ripples.(['TT' num2str(t)]).ripples(:,2));
+        MaxT(t,1) = max(Ripples.(['TT' num2str(t)]).index(:,2));
+        MaxT(t,2) = max(Ripples.(['TT' num2str(t)]).ripples(:,2));
+        end
     end
     M = max(MaxT(:,1));
     t = max(MaxT(:,2));
@@ -34,16 +36,18 @@ for i=1:size(SessionList,1)
     RippleArray = zeros(M,size(TargetTT,1));
     
     for t1=1:length(TargetTT)
+        try
         t=TargetTT(t1);
-        C = Ripples.Ripples.(['TT' num2str(t)]).index;
+        C = Ripples.(['TT' num2str(t)]).index;
         for i=1:size(C,1)
             RippleArray(C(i,1):C(i,2),t1) = 1;
+        end
         end
     end
     RippleVector = sum(RippleArray,2);
     
     
-    n=1;ripples_index=[]; Params.NumTTthreshold=3;
+    n=1;ripples_index=[]; Params.NumTTthreshold=min(3,length(TargetTT));
     for i=2:size(RippleVector,1)
         if RippleVector(i-1)<Params.NumTTthreshold && RippleVector(i)>=Params.NumTTthreshold
             ripples_index(n,1)=i;
@@ -52,10 +56,34 @@ for i=1:size(SessionList,1)
             n=n+1;
         end
     end
+    
+    % grouping
+    ripples2=[]; ripples_index2=[];
+    i=1;
+    while i<size(ripples_index,1)
+        j=i+1;
+        while ripples_index(j,1) - ripples_index(j-1,2) <= Params_Ripple.groupingInterval*Params_Ripple.Fs
+            j=j+1;
+            if j>size(ripples_index,1), continue; end
+        end
+        ripples_index2 = [ripples_index2;[ripples_index(i,1),ripples_index(j-1,2)]];
+        i=j;
+    end
+    if ripples_index(end,2)~=ripples_index2(end,2)
+        ripples_index2 = [ripples_index2 ; ripples_index(end,1:2)];
+    end
+    ripples_index= ripples_index2;
+    
+    ripples_index(ripples_index(:,2)-ripples_index(:,1) < Params_Ripple.minDuration*Params_Ripple.Fs, :) = [];
+    if ripples_index(end,2)<ripples_index(end,1), ripples_index(end,2) = size(RippleVector,1); end
     ripples_index(ripples_index==0) = 1;
+    
+    %%
     ripples_index(:,3) = time_ori + ripples_index(:,1)/Params_Ripple.Fs;
     ripples_index(:,4) = time_ori + ripples_index(:,2)/Params_Ripple.Fs;
     for i=1:size(ripples_index,1), ripples_index(i,5) = max(RippleVector(ripples_index(i,1):ripples_index(i,2))); end
-    writematrix(ripples_index,[ROOT.Save '\' thisSID '_' thisRegion '.csv'])
+    
+    writematrix(ripples_index,[ROOT.Save '\' thisSID '_' thisRegion '.csv'],'WriteMode', 'overwrite')
+    disp([thisSID '_' thisRegion '.csv is saved!'])
     end
 end
