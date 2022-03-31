5d3aa2da7f0d82778fed21f988d3c614896347f4
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-03-31 19:37:20 +0900

    Update RipplePropertySheet_Lv1_exc.m

--- Analysis Set/sheet_printing/RipplePropertySheet_Lv1_exc.m
+++ Analysis Set/sheet_printing/RipplePropertySheet_Lv1_exc.m
@@ -1,12 +1,12 @@
-function RipplePropertySheet_Lv1_exc(ROOT,ID,ripples,EEG,TargetTT,Spike,clusters)
+function RipplePropertySheet_Lv1_exc(ROOT,SList,ripples,EEG,TargetTT,Spike,clusters,Params_Ripple)
 unit = 5.0000e-04;
 EEGpos = [.05 .95 .9 .03]; 
 microSEC = 1e-06;
 len = 10000;
 
-RID = ID(1:3);
-SID = ID(5:6);
 
+RID = jmnum2str(SList.rat,3);
+SID = jmnum2str(SList.session,2);
 %Load Epoch information
 cd([ROOT.Raw.Mother '\rat' RID]);
 
@@ -33,7 +33,7 @@ for epoc = 1: size(thisEPOCH,1)-1
     sheet = figure;
     subplot('position', [.0 .96 .95 .03]);
     D = datetime;
-    text(0.02, 0.5, ['Rat' RID '-S' SID ],'FontSize',14);
+    text(0.02, 0.5, ['Rat' RID '-' SID '-' SList.experimenter{1} '-' SList.type{1} '-d' num2str(SList.day)],'FontSize',14);
     text(0.85, 0.5, ['printed on ' datestr(D,'mmm-dd-yyyy')],'FontSize',10);
     axis off
     
@@ -46,22 +46,58 @@ for epoc = 1: size(thisEPOCH,1)-1
     end
     
     for t=1:nTT
-        subplot('position', EEGpos - [0 .03 0 0]*t)
-        y = EEG.(['TT' num2str(TargetTT(t))]).Raw(thisEPOCH(epoc,1):thisEPOCH(epoc+1,1));
-        plot(y,'k')
-        xlim([0 size(y,1)])
-        axis off
-        if ~isempty(thisRIPs)
-            for rip = 1:size(thisRIPs,1)
-                x = [ripples.Var1(thisRIPs(rip)), ripples.Var2(thisRIPs(rip))] - thisEPOCH(epoc,1);
-                p = patch([x(1) x(2) x(2) x(1)], [min(y) min(y) max(y) max(y)],'r');
-                p.FaceAlpha = 0.3;
-                p.EdgeAlpha = 0;
+        try
+            y = EEG.(['TT' num2str(TargetTT(t))]).Raw(thisEPOCH(epoc,1):thisEPOCH(epoc+1,1));
+            subplot('position', EEGpos - [0 .03 0 0]*t)
+            plot(y,'k')
+            xlim([0 size(y,1)])
+            axis off
+            if ~isempty(thisRIPs)
+                for rip = 1:size(thisRIPs,1)
+                    if ripples.ensemble(thisRIPs(rip))>=3, c='r'; else, c='k'; end
+                    x = [ripples.Var1(thisRIPs(rip)), ripples.Var2(thisRIPs(rip))] - thisEPOCH(epoc,1);
+                    p = patch([x(1) x(2) x(2) x(1)], [min(y) min(y) max(y) max(y)],c);
+                    p.FaceAlpha = 0.3;
+                    p.EdgeAlpha = 0;
+                end
             end
         end
     end
-
-    Spkpos = EEGpos - [0 .03 0 0]*t;
+            %% remove noise period using session threshold
+            envelope_smoothed = EEG.(['TT' num2str(TargetTT(t))]).Envelope_smoothed(epochST(2):epochED(2));
+            temp_stat(1) = mean(envelope_smoothed);
+            temp_stat(2) = std(envelope_smoothed,1);
+            
+            Params_Ripple.threshold(1) = temp_stat(1)+ Params_Ripple.noiseSTD * temp_stat(2);
+            
+            aboveNoiseThreshold = find(envelope_smoothed > Params_Ripple.threshold(1));
+            
+            envelope_smoothed_noiseRemoved = envelope_smoothed;
+            envelope_smoothed_noiseRemoved(aboveNoiseThreshold,1) = NaN;
+            
+            %
+            Params_Ripple.envelope_stat(1) = nanmean(envelope_smoothed_noiseRemoved);
+            Params_Ripple.envelope_stat(2) = nanstd(envelope_smoothed_noiseRemoved,1);
+            
+            Params_Ripple.threshold(2) = Params_Ripple.envelope_stat(1) + Params_Ripple.thresholdSTD * Params_Ripple.envelope_stat(2);
+            y = EEG.(['TT' num2str(TargetTT(t))]).Filtered(thisEPOCH(epoc,1):thisEPOCH(epoc+1,1));
+            subplot('position', EEGpos - [0 .03 0 0]*t)
+            plot(y,'b')
+            line([0 len],[Params_Ripple.threshold(2) Params_Ripple.threshold(2)],'color','k')
+            xlim([0 size(y,1)])
+            axis off
+            if ~isempty(thisRIPs)
+                for rip = 1:size(thisRIPs,1)
+                    if ripples.ensemble(thisRIPs(rip))>=3, c='r'; else, c='k'; end
+                    x = [ripples.Var1(thisRIPs(rip)), ripples.Var2(thisRIPs(rip))] - thisEPOCH(epoc,1);
+                    p = patch([x(1) x(2) x(2) x(1)], [min(y) min(y) max(y) max(y)],c);
+                    p.FaceAlpha = 0.3;
+                    p.EdgeAlpha = 0;
+                end
+            end
+    
+    %%
+    Spkpos = EEGpos - [0 .03 0 0]*(t+1);
 
     
     spks_all=[];u=0; Units={};
@@ -69,22 +105,25 @@ for epoc = 1: size(thisEPOCH,1)-1
         thisTTID = num2str(clusters.TT(un));
         thisCLID = num2str(str2double(clusters.ID{un}(end-1:end)));
         Spk = Spike.(['TT' (thisTTID)]).(['Unit' thisCLID]);
+
         thisSpks = Spk.t_spk(Spk.t_spk>thisEPOCH(epoc,2) & Spk.t_spk<=thisEPOCH(epoc+1,2));
         if ~isempty(thisSpks)
             spks_all = [spks_all;[thisSpks,ones(size(thisSpks,1),1)*u]];
             u=u+1;
-            Units = [Units; ['TT' thisTTID '-' thisCLID]];
+            Units = [Units; [thisTTID '-' thisCLID]];
         end
     end
-    subplot('position',Spkpos -[0 .2 0 -.15])
+    subplot('position',Spkpos -[0 .17 0 -.12])
     hold on
+    
     for s=1:size(spks_all,1)
         x = (spks_all(s,1) - thisEPOCH(epoc,2))/unit;
         patch([x x+ti x+ti x], [spks_all(s,2)+.2 spks_all(s,2)+.2 spks_all(s,2)+.8 spks_all(s,2)+.8],'k')
     end
     for rip = 1:size(thisRIPs,1)
+        if ripples.ensemble(thisRIPs(rip))>=3, c='r'; else, c='k'; end
         x = [ripples.Var1(thisRIPs(rip)), ripples.Var2(thisRIPs(rip))] - thisEPOCH(epoc,1);
-        p = patch([x(1) x(2) x(2) x(1)], [0 0 u u],'r');
+        p = patch([x(1) x(2) x(2) x(1)], [0 0 u u],c);
         p.FaceAlpha = 0.3;
         p.EdgeAlpha = 0;
     end
@@ -98,7 +137,7 @@ for epoc = 1: size(thisEPOCH,1)-1
     set(gca,'fontsize',5)
     
    cd(ROOT.Save)
-saveImage(sheet, ['rat' ID '_' num2str(epoc) '.png'], 'centimeters', sheetPOS); 
+saveImage(sheet, ['rat' RID '-' SID '_' num2str(epoc) '.png'], 'centimeters', sheetPOS); 
 end
 
 
