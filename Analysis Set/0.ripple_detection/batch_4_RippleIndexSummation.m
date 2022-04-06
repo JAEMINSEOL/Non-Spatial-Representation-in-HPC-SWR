9b1c66974d3757af51295c5efd4017a08707a3be
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-04-06 21:07:20 +0900

    Update batch_4_RippleIndexSummation.m

--- Analysis Set/0.ripple_detection/batch_4_RippleIndexSummation.m
+++ Analysis Set/0.ripple_detection/batch_4_RippleIndexSummation.m
@@ -2,13 +2,13 @@
 Initial_SWRFilter_common;
 warning off
 ROOT.Old = [ROOT.Mother '\Processed Data\ripples_mat\R0'];
-ROOT.Save = [ROOT.Mother '\Processed Data\ripples_mat\ProfilingSheet\R0'];
+ROOT.Save = [ROOT.Mother '\Processed Data\ripples_mat\ProfilingSheet\R0_5'];
 %%
 Recording_region = readtable([ROOT.Info '\Recording_region_SWR.csv'],'ReadRowNames',true);
 SessionList = readtable([ROOT.Info '\SessionList_SWR.xlsx'],'ReadRowNames',false);
 Cluster_List = readtable([ROOT.Info '\ClusterList_SWR_CA1_filtered.xlsx']);
 thisRegion = 'CA1';
-Experimenter = {'LSM','JS','SEB'};
+Experimenter = {'LSM','SEB','JS'};
 
 
 
@@ -17,29 +17,32 @@ fd = dir(ROOT.Save);
 RipplesTable_all = table;
 RipplesTable_all = readtable([ROOT.Info '\RipplesList_' thisRegion '.xlsx']);
 %%
-for sid=1:size(SessionList,1)
+for sid=32:size(SessionList,1)
     if SessionList.include(sid) & ismember(SessionList.experimenter{sid},Experimenter) 
         ID = [jmnum2str(SessionList.rat(sid),3) '-' jmnum2str(SessionList.session(sid),2)];
     Recording_region_TT = Recording_region({ID},:);
+        thisRID = SessionList.rat(sid);
+    thisSID = SessionList.session(sid);
     
-    TargetTT = find(cellfun(cellfind(thisRegion),table2array(Recording_region_TT)'));
+    TargetTT = find(cellfun(cellfindn2(thisRegion),table2array(Recording_region_TT)'));
     if size(TargetTT,1)<2, continue; end
     load([ROOT.Old '\' ID '.mat']);
     %%
-    MaxT = [];
     for t1=1:length(TargetTT)
         try
-        t=TargetTT(t1);
-        MaxT(t,1) = length(Ripples.(['TT' num2str(t)]).index);
-        MaxT(t,2) = max(Ripples.(['TT' num2str(t)]).ripples(:,1));
+        thisTTID=TargetTT(t1);
+          L = length(Ripples.(['TT' num2str(thisTTID)]).index);
+            cscID = [jmnum2str(thisRID,3) '-' jmnum2str(thisSID,2) '-' num2str(thisTTID)];
+            % load CSC data
+            cscData = loadCSC(cscID, ROOT.Raw.Mother, Params.CSCfileTag, Params.exportMODE, Params.behExtraction,'CSC');
+            Timestamps_expand = cscData.Timestamps ./ 10^6; % make to sec unit
+            break;
         end
     end
-    if isempty(MaxT), continue; end
-    M = max(MaxT(:,1));
-    t = max(MaxT(:,2));
-    time_ori = t - M/Params_Ripple.Fs;
+
+    time_ori = Timestamps_expand(1);
     
-    RippleArray = zeros(M,size(TargetTT,1));
+    RippleArray = zeros(L,size(TargetTT,1));
     %%
     for t1=1:length(TargetTT)
         try
@@ -50,7 +53,7 @@ for sid=1:size(SessionList,1)
     RippleVector = sum(RippleArray,2);
     %%
     
-    n=1;ripples_index=[]; Params.NumTTthreshold=min(3,length(TargetTT));
+    n=1;ripples_index=[]; Params.NumTTthreshold=max(3,floor(length(TargetTT)/2));
     for i=2:size(RippleVector,1)
         if RippleVector(i-1)<Params.NumTTthreshold && RippleVector(i)>=Params.NumTTthreshold
             ripples_index(n,1)=i;
@@ -61,6 +64,9 @@ for sid=1:size(SessionList,1)
     end
     
     if isempty(ripples_index), disp([ID ' has no ripple!']); continue; end
+    
+        % duration check
+    ripples_index(ripples_index(:,2)-ripples_index(:,1) < Params_Ripple.minDuration*Params_Ripple.Fs, :) = [];
     
     % grouping
     ripples2=[]; ripples_index2=[];
@@ -79,7 +85,8 @@ for sid=1:size(SessionList,1)
     end
     ripples_index= ripples_index2;
     
-    ripples_index(ripples_index(:,2)-ripples_index(:,1) < Params_Ripple.minDuration*Params_Ripple.Fs, :) = [];
+        % duration check
+    ripples_index(ripples_index(:,2)-ripples_index(:,1) < Params_Ripple.minDuration*Params_Ripple.Fs*2, :) = [];
     if ripples_index(end,2)<ripples_index(end,1), ripples_index(end,2) = size(RippleVector,1); end
     ripples_index(ripples_index==0) = 1;
     
@@ -88,36 +95,24 @@ for sid=1:size(SessionList,1)
     ripples_index(:,4) = time_ori + ripples_index(:,2)/Params_Ripple.Fs;
     for i=1:size(ripples_index,1), ripples_index(i,5) = max(RippleVector(ripples_index(i,1):ripples_index(i,2))); end
     
-    writematrix(ripples_index,[ROOT.Old '\' ID '_' thisRegion '.csv'],'WriteMode', 'overwrite')
-    disp([ID '_' thisRegion '.csv is saved!'])
+%     writematrix(ripples_index,[ROOT.Old '\' ID '_' thisRegion '.csv'],'WriteMode', 'overwrite')
+%     disp([ID '_' thisRegion '.csv is saved!'])
+
     
-  
+    ripples = readtable([ROOT.Old '\' ID '_' thisRegion '.csv']);
+    Spike=LoadSpikeData(ROOT, ID, TargetTT, cellfindn);
+    clusters = Cluster_List(Cluster_List.rat==thisRID & Cluster_List.session==thisSID,:);
     %%
-        thisRID = SessionList.rat(sid);
-        thisSID = SessionList.session(sid);
-        ID = [jmnum2str(thisRID,3) '-' jmnum2str(thisSID,2)];
-
-        ripples = readtable([ROOT.Old '\' ID '_' thisRegion '.csv']);
-        EEG = LoadEEGData(ROOT, ID, TargetTT,Params,Params_Ripple);
-            Spike=LoadSpikeData(ROOT, ID, TargetTT, cellfindn);
-            
-        clusters = Cluster_List(Cluster_List.rat==thisRID & Cluster_List.session==thisSID,:);
-       RipplePropertySheet_Lv1_exc(ROOT,SessionList(sid,:),ripples,EEG,TargetTT,Spike,clusters,Params_Ripple);
-       
-       disp([ID ' profile sheet is finished!'])
-
-       %%
-       Ripple_List=table;
-       spks_all=[];u=0; Units={};
+    Ripple_List=table;
+    spks_all=[];
     for cl = 1:size(clusters,1)
         thisTTID = num2str(clusters.TT(cl));
         thisCLID = num2str(str2double(clusters.ID{cl}(end-1:end)));
         Spk = Spike.(['TT' (thisTTID)]).(['Unit' thisCLID]);
-            spks_all = [spks_all;[Spk.t_spk,ones(size(Spk.t_spk,1),1)*cl]];
-            Units = [Units; ['TT' thisTTID '-' thisCLID]];
+        spks_all = [spks_all;[Spk.t_spk,ones(size(Spk.t_spk,1),1)*cl]];
     end
     
-       for rid = 1: size(ripples,1)
+    for rid = 1: size(ripples,1)
         Ripple_List.ID{rid} = [jmnum2str(thisRID,3) '-' jmnum2str(thisSID,2) '-' thisRegion '-' jmnum2str(rid,4)];
         Ripple_List.rat(rid) = thisRID;
         Ripple_List.session(rid) = thisSID;
@@ -136,13 +131,21 @@ for sid=1:size(SessionList,1)
         else
             Ripple_List.ensemble(rid) = 0;
         end
-       
-       end
-       
-       RipplesTable_all = [RipplesTable_all;Ripple_List];
+        
+    end
+    
+    RipplesTable_all = [RipplesTable_all;Ripple_List];
+    
+    %%
+    ripples.ensemble = Ripple_List.ensemble;
+    ID = [jmnum2str(thisRID,3) '-' jmnum2str(thisSID,2)];
+    EEG = LoadEEGData(ROOT, ID, TargetTT,Params,Params_Ripple);
+    RipplePropertySheet_Lv1_exc(ROOT,SessionList(sid,:),ripples,EEG,TargetTT,Spike,clusters,Params_Ripple);
+    
+       disp([ID ' profile sheet is finished!'])
     end
 end
 %%
-writetable(RipplesTable_all,[ROOT.Info '\RipplesList_' thisRegion '.xlsx'],'writemode','overwrite')
-RipplesTable_all = RipplesTable_all(RipplesTable_all.ensemble>=3,:);
-writetable(RipplesTable_all,[ROOT.Info '\RipplesList_' thisRegion '_filtered.xlsx'],'writemode','overwrite')
+% writetable(RipplesTable_all,[ROOT.Info '\RipplesList_' thisRegion '.xlsx'],'writemode','overwrite')
+% RipplesTable_all = RipplesTable_all(RipplesTable_all.ensemble>=3,:);
+% writetable(RipplesTable_all,[ROOT.Info '\RipplesList_' thisRegion '_filtered.xlsx'],'writemode','overwrite')
