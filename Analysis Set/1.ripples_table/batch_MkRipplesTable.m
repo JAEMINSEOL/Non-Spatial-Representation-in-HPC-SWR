8979eb506cdb63abc1babd1153fde6e84650775a
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-05-02 17:25:34 +0900

    Update batch_MkRipplesTable.m

--- Analysis Set/1.ripples_table/batch_MkRipplesTable.m
+++ Analysis Set/1.ripples_table/batch_MkRipplesTable.m
@@ -1,34 +1,56 @@
 Initial_SWRFilter_common;
 warning off
 ROOT.Old = [ROOT.Mother '\Processed Data\ripples_mat\R0'];
-ROOT.Save = [ROOT.Mother '\Processed Data'];
+ROOT.Save = [ROOT.Mother '\Processed Data\ripples_mat\R1'];
 
 Recording_region = readtable([ROOT.Info '\Recording_region_SWR.csv'],'ReadRowNames',true);
 SessionList = readtable([ROOT.Info '\SessionList_SWR.xlsx'],'ReadRowNames',false);
+RippleList_old = readtable([ROOT.Old '\RipplesList_CA1_filtered.xlsx'],'ReadRowNames',false);
 thisRegion = 'CA1';
-Exprimenter = ['LSM'];
+Experimenter = {'LSM','JS','SEB'};
 
-
+RippleList = RippleList_old;
 
 fd = dir(ROOT.Old);
 
 RipplesTable_all = table;
+thisSID_old = '';
 
-for i=1:size(SessionList,1)
+RippleList.RippleDuration=(RippleList.EDtime-RippleList.STtime);
+for sid=1:size(RippleList,1)
     
-    if SessionList.include(i) && ismember(SessionList.experimenter(i),Exprimenter)
-        thisSID = [jmnum2str(SessionList.rat(i),3) '-' jmnum2str(SessionList.session(i),2)];
-        load([ROOT.Old '\' thisSID '.mat']);
+    if ismember(RippleList.experimenter(sid),Experimenter)
+        thisSID = [jmnum2str(RippleList.rat(sid),3) '-' jmnum2str(RippleList.session(sid),2)];
+        Recording_region_TT = Recording_region({thisSID},:);
+        TargetTT = find(cellfun(cellfind(thisRegion),table2array(Recording_region_TT)'));
         
-        Rip = MkRipplesTable(ROOT,thisSID,thisRegion);
+        if ~strcmp(thisSID, thisSID_old)
+            disp([thisSID ' EEG data loading...'])
+            EEG = LoadEEGData(ROOT, thisSID, TargetTT,Params,Params_Ripple);
+            thisSID_old = thisSID;
+        end
         
-%         RipplesTable_all = [RipplesTable_all; Rip];
-        writetable(Rip,[ROOT.Save '\ripples_mat\R1\RipplesTable_' thisSID '_' thisRegion '.xlsx'], 'overwritesheet')
-%         save([ROOT.Save '\ripples_mat\R1\' thisSID '_' thisRegion '.mat'], 'Rip')
+        EEG_Prop=struct;
+        Idx = [RippleList.STindex(sid):RippleList.EDindex(sid)];
+        for t=1:length(TargetTT)
+            try
+            thisEEG = EEG.(['TT' num2str(TargetTT(t))]);
+            [EEG_Prop.Amp(t), EEG_Prop.Freq(t), EEG_Prop.power_ripple(t), EEG_Prop.power_theta(t)] = LFP_Properties(thisEEG,Idx);
+            catch
+                 EEG_Prop.Amp(t) = nan; EEG_Prop.Freq(t)= nan; EEG_Prop.power_ripple(t)= nan; EEG_Prop.power_theta(t)= nan;
+            end
+        end
+        RippleList.MaxVoltage(sid) = nanmax(EEG_Prop.Amp);
+        RippleList.MeanVoltage(sid) = nanmean(EEG_Prop.Amp);
+        RippleList.MaxFreq(sid) = nanmax(EEG_Prop.Freq);
+        RippleList.MeanFreq(sid) = nanmean(EEG_Prop.Freq);
+        RippleList.RipplePower(sid) = nanmean(EEG_Prop.power_ripple);
+        RippleList.ThetaPower(sid) = nanmean(EEG_Prop.power_theta);
+%         writetable(Rip,[ROOT.Save '\RipplesTable_' thisSID '_' thisRegion '.xlsx'], 'overwritesheet')
     end
     
 end
-
+writetable(RippleList,[ROOT.Save '\RipplesTable_' thisRegion '.xlsx'],'WriteMode', 'overwrite')
 %  load([ROOT.Save '\RipplesTable.mat'])
 
 
