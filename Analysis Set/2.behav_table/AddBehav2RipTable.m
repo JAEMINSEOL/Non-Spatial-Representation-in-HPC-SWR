7a4e51d914f84694747a5287fb54ccc4ed0a1dc8
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-05-05 10:50:00 +0900

    Update AddBehav2RipTable.m

--- Analysis Set/2.behav_table/AddBehav2RipTable.m
+++ Analysis Set/2.behav_table/AddBehav2RipTable.m
@@ -1,9 +1,34 @@
 
+Initial_SWRFilter_common;
+warning off
+ROOT.Old = [ROOT.Mother '\Processed Data\ripples_mat\R1'];
+ROOT.Save = [ROOT.Mother '\Processed Data\ripples_mat\R2'];
+ROOT.Behav = [ROOT.Mother '\Processed Data\behavior_mat'];
 
-for sid=1:2  
+Recording_region = readtable([ROOT.Info '\Recording_region_SWR.csv'],'ReadRowNames',true);
+SessionList = readtable([ROOT.Info '\SessionList_SWR.xlsx'],'ReadRowNames',false);
+RippleList_old = readtable([ROOT.Old '\RipplesTable_CA1.xlsx'],'ReadRowNames',false);
+
+
+thisRegion = 'CA1';
+Experimenter = {'LSM','JS','SEB'};
+
+RipplesTable = RippleList_old;
+
+fd = dir(ROOT.Old);
+
+RipplesTable_all = table;
+thisSID_old = '';
+
+RipplesTable.RippleDuration=(RipplesTable.EDtime-RipplesTable.STtime);
+for sid=1:size(RipplesTable,1)
+
+        thisSID = [jmnum2str(RipplesTable.rat(sid),3) '-' jmnum2str(RipplesTable.session(sid),2)];
+          load([ROOT.Behav '\' thisSID '.mat']);
+          BehavTable = readtable([ROOT.Behav '\' thisSID '.xlsx']);
     %% add to RipplesTable
-    Idx = knnsearch(Behav.t,RipplesTable.StartTime(sid));
-    Idx_t = find(Behav.trial_time(:,1)<RipplesTable.StartTime(sid));
+    Idx = knnsearch(Behav.t,RipplesTable.STtime(sid));
+    Idx_t = find(Behav.trial_time(:,1)<RipplesTable.STtime(sid));
     
     
     RipplesTable.PosX(sid) = Behav.x(Idx);
@@ -20,15 +45,15 @@ for sid=1:2
         RipplesTable.correctness(sid) = Behav.trial_correctness(Idx_t);
         RipplesTable.ambiguity(sid) = Behav.trial_ambiguity(Idx_t);
         RipplesTable.area(sid) = Behav.trial_vector(Idx,2);
-        RipplesTable.StartTime_fromTrialEnd(sid) = RipplesTable.StartTime(sid) - BehavTable.end(Idx_t);
-        RipplesTable.EndTime_fromTrialEnd(sid) = RipplesTable.EndTime(sid) - BehavTable.end(Idx_t);
+        RipplesTable.StartTime_fromTrialEnd(sid) = RipplesTable.STtime(sid) - BehavTable.xEnd(Idx_t);
+        RipplesTable.EndTime_fromTrialEnd(sid) = RipplesTable.EDtime(sid) - BehavTable.xEnd(Idx_t);
     else
         RipplesTable.trial{sid} = [thisSID '-000'];
         RipplesTable.StartTime_fromTrialEnd(sid) = nan;
         RipplesTable.EndTime_fromTrialEnd(sid) = nan;
     end
 
-    
+
 end
 
 RipplesTable = RipplesTable(RipplesTable.StartTime_fromTrialEnd>0,:);
