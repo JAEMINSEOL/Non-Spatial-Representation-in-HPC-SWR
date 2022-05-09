052de9c560ba0823ea43427e0fccfcbbd8fbb23b
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-05-09 11:17:12 +0900

    Update MkBehavEpoch.m

--- Analysis Set/position_parsing/MkBehavEpoch.m
+++ Analysis Set/position_parsing/MkBehavEpoch.m
@@ -1,24 +1,26 @@
 % WriteBehaviorEpoch.csv using Parsed Position
 
 function MkBehavEpoch(ROOT,Session_List,sid)
-        clear t
+        clear Behav
+        Epoch=zeros(15,2);
         thisRID = Session_List.rat(sid);
         thisSID = Session_List.session(sid);
         try
             loc = [ROOT.Raw.Mother '\rat' jmnum2str(thisRID,3) '\rat' jmnum2str(thisRID,3) '-' jmnum2str(thisSID,2)];
-            load([loc '\ParsedPosition.mat'])
+            Behav = load([loc '\ParsedPosition.mat']);
 %             d = dir([loc '\Behavior\ParsedPosition.mat']);
 %             cd([loc '\Behavior'])
 %             copyfile(d.name, [loc '\ParsedPosition.mat'])
-            Epoch(thisSID,1) = t(1);
-            Epoch(thisSID,2) = t(end);
+            Epoch(thisSID,1) = Behav.t(1);
+            Epoch(thisSID,2) = Behav.t(end);
         catch
         end
         
         
         if Session_List.rat(sid+1)~=thisRID
             xlswrite([ROOT.Raw.Mother '\rat' jmnum2str(thisRID,3) '\behaviorEpoch_rat' jmnum2str(thisRID,3) '.xlsx'],Epoch)
-            Epoch=zeros(15,2);
             disp(['behaviorEpoch_rat' jmnum2str(thisRID,3) '.xlsx is saved!'])
         end
     end
+
+        
