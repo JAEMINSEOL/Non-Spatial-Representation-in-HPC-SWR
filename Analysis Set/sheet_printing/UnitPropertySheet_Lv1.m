0940235667a89538764b3ac1e9c6ff484c98f262
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-09-16 15:23:04 +0900

    Update UnitPropertySheet_Lv1.m

--- Analysis Set/sheet_printing/UnitPropertySheet_Lv1.m
+++ Analysis Set/sheet_printing/UnitPropertySheet_Lv1.m
@@ -8,17 +8,21 @@ Session_List = readtable([ROOT.Info '\SessionList_SWR.xlsx']);
 Recording_region = readtable([ROOT.Info '\Recording_region_SWR.csv']);
 
 Cluster_List = readtable([ROOT.Info '\ClusterList_SWR_CA1_filtered.xlsx']);
-exper = {'JS','SEB','LSM'};
+exper = {'JS','SEB'};
 TargRegion = 'CA1';
 
-for cid = 452:size(Cluster_List,1)
+for cid = 1:size(Cluster_List,1)
 if ismember(Cluster_List.experimenter{cid},exper) 
         clusterID = Cluster_List.ID{cid};
         sid = find(Session_List.rat==Cluster_List.rat(cid) & Session_List.session==Cluster_List.session(cid));
+        try
 [fig,nspks,onmazeAvgFR] = JMGetClusterQuals_4sheet(clusterID, ROOT,Session_List.type{sid},Cluster_List.experimenter{cid});
 Cluster_List.onMazeAvgFR(cid) = onmazeAvgFR;
 Cluster_List.nSpks(cid) = nspks;
+        catch
+            disp([clusterID ' is failed!'])
+        end
 end
 end
 
-writetable(Cluster_List_CA1,[ROOT.Info '\ClusterList_SWR_CA1_filtered.xlsx'],'WriteMode', 'replacefile')
+% writetable(Cluster_List_CA1,[ROOT.Info '\ClusterList_SWR_CA1_filtered.xlsx'],'WriteMode', 'replacefile')
