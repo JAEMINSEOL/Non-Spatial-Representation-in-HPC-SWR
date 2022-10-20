4465b2ef04446ad423e439d6a7eb1ed358e0213b
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-10-20 18:15:05 +0900

    Update MkUnitTable_batch.m

--- Analysis Set/3.units_table/MkUnitTable_batch.m
+++ Analysis Set/3.units_table/MkUnitTable_batch.m
@@ -14,53 +14,65 @@ thisRegion = 'CA1';
 
 UnitsTable_all =table;
 
-Cluster_List = readtable([ROOT.Info '\ClusterList_SWR_CA1_filtered.xlsx']);
-exper = {'JS','SEB','LSM'};
+Cluster_List = readtable([ROOT.Save '\units_mat\U0\ClusterList_SWR_CA1_filtered.xlsx']);
+exper = {'JS','LSM','SEB'};
+exper2 = {'JS','LSM'};
+% exper = {'SEB'};
 TargRegion = 'CA1';
 
 
-for i=1:size(fd,1)
-    id = fd(i).name;
-    
-    if length(id)==14
-        thisSID = [id(1:6)];
+for cid=1:size(Cluster_List,1)
+    id = Cluster_List.ID{cid};
+    if ~ismember(Cluster_List.experimenter{cid},exper), continue; end
+ 
+        thisSID = id(1:6);
     
         Recording_region_TT = Recording_region({thisSID},:);
         if strcmp(thisRegion, 'CA3')
-            TargetTT = find(cellfun(cellfindn2(thisRegion),table2array(Recording_region_TT)'));
+            TargetTT = find(cellfun(Params.cellfindn2(thisRegion),table2array(Recording_region_TT)'));
         else
-            TargetTT = find(cellfun(cellfind(thisRegion),table2array(Recording_region_TT)'));
+            TargetTT = find(cellfun(Params.cellfindn2(thisRegion),table2array(Recording_region_TT)'));
         end
         
-        Spike = LoadSpikeData(ROOT, thisSID, TargetTT,cellfindn);
+        Spike = LoadSpikeData(ROOT, thisSID, TargetTT,Params.cellfindn);
         load([ROOT.Behav '\' thisSID '.mat'])
-        UnitsTable = MkUnitTable(ROOT,Behav,Spike,thisSID,thisRegion,TargetTT);
+        
+        UnitsTable = MkUnitTable(ROOT,Behav,Spike,id,thisRegion,TargetTT,Params);
         
         save([ROOT.Unit '\' thisSID '.mat'], 'UnitsTable')
         UnitsTable_all = [UnitsTable_all; UnitsTable];
-    end
+        disp([id ' is finished!'])
 end
 
-writetable(UnitsTable_all,[ROOT.Save '\UnitsTable_' thisRegion '.xlsx']);
+writetable(UnitsTable_all,[ROOT.Unit '\UnitsTable3_' thisRegion '.xlsx']);
 
 %% unit filtering
 
 units = UnitsTable_all;
 % average peak-to-valley amplitude of waveforms >= 75uV 
-id = units.Peak_peakToValley_>=75;
+id = units.AvgPeaktoValley>=75;
 units = units(id,:);
 
 % proportion ofspikes within a 1ms refractory period < 1% of total spikes
-id = units.WithinRefractory_perc_<1;
-units = units(id,:);
-
-% average firing rate during the outbound journey on the stem and arms >= 0.5Hz
-id = units.onmazeAvgFR1DOut>=0.5;
+id = units.withinRef<1;
 units = units(id,:);
 
 % fast-spiking neurons (mean firing rate >=10 Hz; width of the average waveform < 325us) were excluded
-id = units.MeanFR>=10 & units.SpkWidth_maxAmp_<325;
+id = units.AvgFR>=10 & units.SpkWidth<325;
 units = units(~id,:);
 
 UnitsTable_filtered = units;
-writetable(UnitsTable_filtered,[ROOT.Save '\UnitsTable_filtered2_' thisRegion '.xlsx']);
+writetable(UnitsTable_filtered,[ROOT.Unit '\UnitsTable_filtered_' thisRegion '.xlsx']);
+
+% average firing rate during the outbound journey on the stem and arms >= 0.5Hz
+id = units.onMazeAvgFR>=0.5;
+units = units(id,:);
+
+id = ismember(units.experimenter,exper2);
+units = units(id,:);
+
+id = nanmin(units.ReMap_LScene,units.ReMap_RScene,units.ReMap_LR)==0;
+units = units(id,:);
+
+UnitsTable_anal = units;
+writetable(UnitsTable_anal,[ROOT.Unit '\UnitsTable_' thisRegion '_forAnalysis.xlsx']);
