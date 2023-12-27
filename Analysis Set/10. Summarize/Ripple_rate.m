01fd31f8337a7ee2bc14188cbd9bcb9c077d3e23
Jaemin Seol <43691888+JAEMINSEOL@users.noreply.github.com>
2023-12-27 15:53:23 +0900

    Add files via upload

--- Analysis Set/10. Summarize/Ripple_rate.m
+++ Analysis Set/10. Summarize/Ripple_rate.m
@@ -1,13 +1,15 @@
 Initial_SWRFilter_common;
 warning off
-ROOT.Save = [ROOT.Mother '\Processed Data'];
-ROOT.Rip0 = [ROOT.Mother '\Processed Data\ripples_mat\R0'];
-ROOT.Rip = [ROOT.Mother '\Processed Data\ripples_mat\R3'];
-ROOT.Rip4 = [ROOT.Mother '\Processed Data\ripples_mat\R4'];
-ROOT.Rip5 = [ROOT.Mother '\Processed Data\ripples_mat\R5_cell_AllPopul'];
-ROOT.Fig = [ROOT.Mother '\Manuscript figures\R2\R0_fig'];
-ROOT.Units = [ROOT.Mother '\Processed Data\units_mat\U2'];
-ROOT.Behav = [ROOT.Mother '\Processed Data\behavior_mat'];
+ROOT.Save = [ROOT.Processed];
+ROOT.Rip0 = [ROOT.Save '\ripples_mat\R0'];
+ROOT.Rip1 = [ROOT.Save '\ripples_mat\R1'];
+ROOT.Rip2 = [ROOT.Save '\ripples_mat\R2'];
+ROOT.Rip3 = [ROOT.Save '\ripples_mat\R3'];
+ROOT.Rip4 = [ROOT.Save '\ripples_mat\R4'];
+ROOT.Rip5 = [ROOT.Save '\ripples_mat\R5_cell_AllPopul'];
+ROOT.Fig = [ROOT.Save '\Manuscript figures\R2\R0_fig'];
+ROOT.Units = [ROOT.Save '\units_mat\U2'];
+ROOT.Behav = [ROOT.Save '\behavior_mat'];
 
 dir = '';
 
@@ -23,55 +25,66 @@ microSEC = 1e-06;
 len = 20000;
 thisFRMapSCALE=2;
 %%
-thisRegion0 = 'SUB_refCA1';
-thisRegion = 'SUB';
-thisRegion2 = 'SUB_field';
-Sel = 'LScene';
-% RipplesTable = readtable([ROOT.Rip '\RipplesTable_' thisRegion '_forAnalysis.xlsx']);
-% RipplesTable = readtable([ROOT.Save '\RipplesTable_' thisRegion2 '_RDIs_UV_cell_HeteroIn_AllPopul.xlsx']);
 
-RipplesTable = readtable([ROOT.Rip '\RipplesTable_' thisRegion0 '_forAnalysis.xlsx']);
-UnitsTable = readtable([ROOT.Units '\UnitsTable_' thisRegion '_forAnalysis.xlsx']);
+SessionRips=table;
+sid=1;
+%%
+thisRegion0 = 'SUB';
+
+RA = readtable([ROOT.Save '\RipplesTable_' thisRegion0 '_forAnalysis.xlsx']);
+R0 = readtable([ROOT.Rip0 '\RipplesList_' thisRegion0 '.xlsx']);
+R2 = readtable([ROOT.Rip2 '\RipplesTable_Behav_' thisRegion0 '_speed_filtered.xlsx']);
+R3 = readtable([ROOT.Rip3 '\RipplesTable_Behav_Overlap' thisRegion0 '.xlsx']);
+R4 = readtable([ROOT.Rip3 '\RipplesTable_' thisRegion0 '_forAnalysis.xlsx']);
+% RipplesTable = readtable([ROOT.Rip '\RipplesTable_' thisRegion0  '_forAnalysis.xlsx']);
 
 TT_table = readtable([ROOT.Info '\TT_table.xlsx']);
 
-for sid=1:size(SessionList,1)
-% for sid=32:32
-    if ~SessionList.include(sid) | ~strcmp(SessionList.experimenter(sid),'LSM'), continue; end
-    thisRIDn = SessionList.rat(sid); thisRID=jmnum2str(thisRIDn,3);
-    thisSIDn = SessionList.session(sid); thisSID=jmnum2str(thisSIDn,2);
+
+for s=1:size(SessionList,1)
+    % for sid=32:32
+    if ~SessionList.include(s) | ~strcmp(SessionList.experimenter(s),'LSM'), continue; end
+    thisRIDn = SessionList.rat(s); thisRID=jmnum2str(thisRIDn,3);
+    thisSIDn = SessionList.session(s); thisSID=jmnum2str(thisSIDn,2);
     thisRSID = [thisRID '-' thisSID];
-    load([ROOT.Behav '\' thisRSID '.mat']);
+
     BehavTable = readtable([ROOT.Behav '\' thisRSID '.xlsx']);
-    Spike=LoadSpikeData(ROOT, thisRSID, [1:24], Params.cellfindn);
-
-    theseRip = RipplesTable(RipplesTable.rat==thisRIDn & RipplesTable.session==thisSIDn,:);
-
-    if isempty(theseRip), continue; end
 
 
-    Pos = load([ROOT.Raw.Mother '\rat' thisRID '\rat' thisRID '-' thisSID '\ParsedPosition.mat']);
-    diverging_point = get_divergingPoint(ROOT.Info, thisRID, thisSID);
+    theseR0 = R0(R0.rat==thisRIDn & R0.session==thisSIDn,:);
+    theseR2 = R2(R2.rat==thisRIDn & R2.session==thisSIDn,:);
+    theseRA = RA(RA.rat==thisRIDn & RA.session==thisSIDn,:);
+    
+    SessionRips.session{sid} = thisRSID;
+    SessionRips.region{sid} = thisRegion0;
+    SessionRips.time_sum(sid) = sum(BehavTable.duration_ITI(BehavTable.correctness==1));
 
-    diverging_point = diverging_point*0.23;
-    stem_end_index =  (max(Pos.y)-diverging_point)/thisFRMapSCALE;
+    SessionRips.Rips_LFP(sid) = size(theseR0,1);
+    SessionRips.Rips_Overlap(sid) = sum(theseR0.Overlap~=0);
+    SessionRips.Rips_Ensemble(sid) = sum(theseR0.ensemble>2);
+    SessionRips.Rips_O_E(sid) = sum(theseR0.Overlap~=0 & theseR0.ensemble>2);
+    SessionRips.Rips_O_E_Behav(sid) = sum(theseR2.Overlap~=0 & theseR2.ensemble>2);
+    SessionRips.Rips_O_E_B_Pc(sid) = sum(theseRA.Overlap~=0);
 
-    Recording_region_TT = Recording_region(thisRSID,:);
-    TargetTT = GetTargetTT(ROOT,thisRSID,thisRegion,Params,0.03);
-
-    thisTT_table = TT_table(TT_table.rat==thisRIDn & TT_table.session==thisSIDn,:);
-    for t=1:size(thisTT_table,1)
-        if ~ismember(thisTT_table.TT(t),TargetTT)
-            thisTT_table.TT(t)=0;
-        end
-    end
-    thisTT_table= thisTT_table(thisTT_table.TT~=0,:);
-    [~,t] = max(thisTT_table.RippleBandMean);
-    TargetTT_p = thisTT_table.TT(t);
-
-
-
-    EEG = LoadEEGData(ROOT, thisRSID, TargetTT_p,Params,Params_Ripple);
-
-time_sum = sum(BehavTable.duration_ITI(BehavTable.correctness==1))
+    sid=sid+1;
 end
+%%
+writetable(SessionRips,[ROOT.Save '\SessionRips_pre.xlsx'],'WriteMode','replacefile')
+
+%  SessionRips.RippleRate = SessionRips.num_rips./SessionRips.time_sum;
+%%
+SessionRips_sub = SessionRips;
+SessionRips_sub(isnan(SessionRips_sub.RippleRate),:)=[];
+SessionRips_ca1(isnan(SessionRips_ca1.RippleRate),:)=[];
+
+
+ s0 = SessionRips_sub.num_rips;
+  s1 = SessionRips_ca1.num_rips;
+
+   nanmean(s0)
+  nanstd(s0)
+
+ nanmean(s1)
+  nanstd(s1)
+
+ [h,p]= ttest2(s1,s0);
